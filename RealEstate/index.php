de05f494b9eb3c006a8d84793b2dcfdc1f7e8409
Jeffrey Eyram Kweku Agra <agrajeff15@gmail.com>
2025-07-22 07:33:36 +0000

    working(trials) on minor projects

---
+++ RealEstate/index.php
@@ -0,0 +1,22 @@
+<?php
+include('./includes/header.php')
+?>
+
+    <!-- header for project -->
+
+    <header>
+        <div class="house-header">
+            <div class="house-container">
+                <div class="logo">🛒 Buy me</div>
+                <nav>
+                    <ul>
+                        <li><a href="#Home">Home</a></li>
+                        <li><a href="#Property">Property</a></li>
+                        <li><a href="#Land">Land</a></li>
+                        <li><a href="#About">About</a></li>
+                        <li><a href="#Contact">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
